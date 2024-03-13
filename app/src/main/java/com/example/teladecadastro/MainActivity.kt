78c0b99642c5f1e9c69d99a2e5ce1fc20cae3516
Tallia-Sousa <talliasousa.com@gmail.com>
2024-03-13 20:30:00 -0300

    atualizando

--- app/src/main/java/com/example/teladecadastro/MainActivity.kt
+++ app/src/main/java/com/example/teladecadastro/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.teladecadastro
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.ButtonBarLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var editTextTitle: EditText;
+    private lateinit var botaoC: Button
+    private lateinit var botaoL:Button
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,28 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }
+
+        editTextTitle = findViewById(R.id.editTextTitle)
+        botaoC = findViewById(R.id.botaoC)
+        botaoL = findViewById(R.id.botaoL)
+
+        botaoC.setOnClickListener{
+            val userInput = editTextTitle.text.toString()//obter oq o user digitou
+
+            when {
+                userInput.isNotEmpty() ->  Toast.makeText(this, userInput, Toast.LENGTH_SHORT).show()
+                else -> editTextTitle.error = "Por favor, insira um texto"
+
+            }//inicializar
+
+
+            botaoL.setOnClickListener {
+                editTextTitle.text.clear()
+            }
+            }
+
     }
-}
+    }
--- app/src/main/res/readme.md
+++
@@ -1,5 +0,0 @@
-![](app/src/main/res/drawable/trabalho.png)
-
-
-
-
